dc4e62ef07a4b160326746c284b47b6bbd965dcf
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-07 21:09:35 +0900

    TestIndexAction

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -20,6 +20,17 @@ describe TweetsController do
       expect(response).to render_template :edit
     end
   end
-  
+
+  describe 'GET #index' do
+    it "populates an array of tweets ordered by created_at DESC" do
+      tweets = create_list(:tweet, 3)
+      get :index
+      expect(assigns(:tweets)).to match(tweets.sort{ |a,b| b.created_at <=> a.created_at } )
+    end
+
+    it "renders the :index template" do
+    end
+  end
+
 end
 
