ed973f8978b0bc8d75a1ce19d7224d0af1a0b412
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-12 15:59:20 +0900

    AddSampleTest

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 feature 'tweet', type: :feature do
+  given(:user) { create(:user) }
+
+  scenario 'ユーザー情報が更新されていること' do
+    # ログイン前には投稿ボタンがない
+    visit root_path
+    expect(page).to have_no_content('投稿する')
+  end
 end
